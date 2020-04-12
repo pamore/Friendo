e21a4674c4b5a7c46e7a726fefc3293fec086718
prachi.more1 <prachi.more@integral.io>
2020-04-12 19:53:20 -0400

    Extracted note component.

--- src/component/DashboardView.js
+++ src/component/DashboardView.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import {NoteComponent} from "./NoteComponent";
 
 export default function DashboardView() {
     return(
         <Grid
             xs={12}
             className="dashboard"
-            style={{background: theme.ui, color: theme.syntax}}
         >
-            <Paper elevation={7} className="note"/>
-            <Paper elevation={7} className="note"/>
-            <Paper elevation={7} className="note"/>
+            <NoteComponent />
+            <NoteComponent />
         </Grid>
     );
 }
---
+++ src/component/NoteComponent.js
@@ -0,0 +1,6 @@
+import Paper from "@material-ui/core/Paper";
+import React from "react";
+
+export function NoteComponent() {
+    return <Paper elevation={7} className="note"/>;
+}
