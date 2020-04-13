0a9fe3b00b7fb299d803b9f0e7fa0e6052e4358d
prachi.more1 <prachi.more@integral.io>
2020-04-12 20:12:18 -0400

    Added loop to create notes.

--- src/component/DashboardView.js
+++ src/component/DashboardView.js
@@ -1,16 +1,45 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import {NoteComponent} from "./NoteComponent";
 
 export default function DashboardView() {
-    return(
+    return (
         <Grid
             xs={12}
             className="dashboard"
         >
-            <NoteComponent />
-            <NoteComponent />
+            {notes.map((note) => (
+                <NoteComponent
+                    note = {note}
+                />
+            ))}
         </Grid>
     );
 }
+
+const notes = [
+    {
+        id: 1,
+        title: "Note 1",
+        description: "Description is small.",
+        complete: false
+    },
+    {
+        id: 2,
+        title: "Note 2",
+        description: "Description is small too.",
+        complete: true
+    },
+    {
+        id: 3,
+        title: "Note 3",
+        description: "Description is what it is.",
+        complete: false
+    },
+    {
+        id: 4,
+        title: "Note 4",
+        description: "Description 4.",
+        complete: false
+    }
+];
--- src/component/NoteComponent.js
+++ src/component/NoteComponent.js
@@ -1,6 +1,20 @@
 import Paper from "@material-ui/core/Paper";
 import React from "react";
+import Checkbox from '@material-ui/core/Checkbox';
 
-export function NoteComponent() {
-    return <Paper elevation={7} className="note"/>;
+export function NoteComponent(props) {
+    const {id, title, description, complete} = props.note;
+    return (
+        <Paper
+            elevation={7}
+            className="note"
+        >
+            <p>id : {id}</p>
+            <p>title : {title}</p>
+            <p>description : {description}</p>
+            <Checkbox
+                checked={complete}
+            />complete
+        </Paper>
+    );
 }
